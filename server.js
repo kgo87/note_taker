ffe6b50fcbe6b704f2e99d0497b808b4241f6dc8
Kat Gomozova <kathy.gomozova@gmail.com>
2020-12-16 18:39:27 -0800

    second commit

--- server.js
+++ server.js
@@ -14,42 +14,75 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Data
-var notes = db
-// [{"title":"Test Title","text":"Test text"}]
+var notes = []
+
 
 // Routes
 // ===========================================================
+
+app.get('/api/notes' ,function (req , res) {
+  console.log(db)
+  return res.json(db);
+  
+});
+
+// app.get("/api/notes", function(req, res) {
+//     fs.readFile(path.join(__dirname,"db","db.json"), function(error, data){
+//         if(error) {
+//             throw new error
+//         }
+//         else {
+//             res.send(data)
+//         }
+//     })
+// });
+// fs to red -> then parse with JSON.parse(). Send the parsed data with res.json()
+
+
+// Create New Note 
+app.post("/api/notes", function(req, res) {
+
+    var note = req.body;
+    // read file using fs module and parse to array with JSON.parse -> push req.body to array list 
+    // Json.stringify array list to JSON file
+    // save the contents back to db.json file with FS module
+    
+    console.log(note);
+    console.log(notes)
+    parsedNote = JSON.stringify(note);
+    db.push(parsedNote);
+    console.log(db)
+
+  
+    notes.push(note);
+    res.json(note);
+  });
+
+  app.delete('/api/notes/:id' ,function (req , res) {
+    // use fs to read the file and parse the data
+    // use array.splice and find the id using for loop 
+    // req.params.id
+    // Option B: use filter method to find the index and remove element with that index
+    // return any type of success method (retrun true/false) 
+
+    return res.json(db);
+    
+  });
+
+// [{"title":"Test Title","text":"Test text"}]
+
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname,"public", "index.html"));
 });
+
+
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname,"public", "notes.html"))
 });
 
-app.get("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname,"db","db.json"), function(error, data){
-        if(error) {
-            throw new error
-        }
-        else {
-            res.send(data)
-        }
-    })
-});
 
-// Create New Note 
-app.post("/api/notes", function(req, res) {
-    // var newNoteTitle = req.title;
-    // var newNoteBody = req.text;
-    var note = req.body;
-    console.log(note);
-
-  
-    notes.push(note);
-  
-    res.json(note);
-  });
 
 // Listener
 // ===========================================================
